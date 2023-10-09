bb897ce40bd3c692d8ace9f1596a720f94145094
Samuel Draper <samueledraper@outlook.com>
2023-10-09 15:25:12 +0100

    add check song function

--- results/app.js
+++ results/app.js
@@ -9,11 +9,79 @@ function Song(name, artist, mood, genre, album_art_src, yt_src) {
 
 const songs = [
   new Song(
-    "Never Gonna Give You Up",
+    "Never Gonna Give You Up Good",
     "Rick Astley",
     "good",
     "pop",
     "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
     "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
   ),
+  new Song(
+    "Never Gonna Give You Up Bad",
+    "Rick Astley",
+    "bad",
+    "pop",
+    "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
+  ),
+  new Song(
+    "Never Gonna Give You Down Good",
+    "Rick Astley",
+    "good",
+    "rock",
+    "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
+  ),
+  new Song(
+    "Never Gonna Give You Down Bad",
+    "Rick Astley",
+    "bad",
+    "rock",
+    "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
+  ),
+  new Song(
+    "Never Gonna Give You Left Good",
+    "Rick Astley",
+    "good",
+    "Electronic",
+    "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
+  ),
+  new Song(
+    "Never Gonna Give You Left Bad",
+    "Rick Astley",
+    "bad",
+    "Electronic",
+    "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
+  ),
+  new Song(
+    "Never Gonna Give You Right Good",
+    "Rick Astley",
+    "good",
+    "hip-hop",
+    "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
+  ),
+  new Song(
+    "Never Gonna Give You Right Bad",
+    "Rick Astley",
+    "bad",
+    "hip-hop",
+    "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/ed/17/65/ed17656f-4c55-97c2-c93d-4b94f829799f/859381157694.jpg/600x600bb.jpg",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
+  ),
 ];
+
+let matchingSongs = [];
+
+function checkSongs() {
+  for (let i = 0; i < songs.length; i++) {
+    if (songs[i].mood === userProfile.mood && songs[i].genre === userProfile.genre) {
+      matchingSongs.push(songs[i]);
+    } else {
+      console.log("no match");
+    }
+  }
+}
